47bd761f5eb3c48f99519f5526889b310720d439
Chojihyeon <jgh3390@gmail.com>
2017-05-29 21:25:41 +0900

    media 파일과 communication 앱 수정

--- AdsCatcher/communication/models.py
+++ AdsCatcher/communication/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Report(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=30)
+    contents = models.TextField()
+
+    def __str__(self):
+        return '{}, {}'.format(self.title, self.id)
--- AdsCatcher/gallery/urls.py
+++ AdsCatcher/gallery/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import gallery_list
 
+
 urlpatterns = [
      url(r'^$', gallery_list, name="index"),
 ]
