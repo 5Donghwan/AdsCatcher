9d83870074d41eeafb4b03b8b3c487c34f11c60e
Ohdonghwan <Ohdongdong@Ohdonghwanui-MacBook-Pro.local>
2017-05-29 16:46:32 +0900

    gitignore

--- AdsCatcher/gallery/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Gallery
-# Register your models here.
-
-class GalleryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', "author"]
-
-admin.site.register(Gallery, GalleryAdmin)
--- AdsCatcher/gallery/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import index, gallery_new, gallery_list
-
-urlpatterns = [
-     url(r'^$', index, name="index"),
-    url(r'^new/$', gallery_new, name="gallery_new"),
-     url(r'^list/$', gallery_list, name="gallery_list"),
-]
-
