f9b7fdb596878a138ccad68dce1b6d20478c5079
Ohdonghwan <Ohdongdong@Ohdonghwanui-MacBook-Pro.local>
2017-06-05 15:16:13 +0900

    login custom url unactive

--- AdsCatcher/accounts/urls.py
+++ AdsCatcher/accounts/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, {'next_page': 'gallery:index'}, name='logout'),
+    #url(r'^login/$', login, name='login'),
+    #url(r'^logout/$', logout, {'next_page': 'gallery:index'}, name='logout'),
 ]
