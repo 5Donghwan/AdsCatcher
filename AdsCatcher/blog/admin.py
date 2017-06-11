a11c2207274281a697ba2efb5e28496415204805
Chojihyeon <jgh3390@gmail.com>
2017-06-11 23:32:07 +0900

    scraper.py 추가, blog 모델 생성, blog.admin 등록

--- AdsCatcher/blog/admin.py
+++ AdsCatcher/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['page_name', 'feed_title', 'content', 'created_at']
+
+admin.site.register(Blog, BlogAdmin)
--- AdsCatcher/blog/models.py
+++ AdsCatcher/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    page_name = models.CharField(max_length=30)
+    feed_title = models.CharField(max_length=300)
+    content = models.CharField(max_length=600)
+    created_at = models.CharField(max_length=30)
