8bbf5934206c0dab6a8a09cba3c716de3d52873f
5Donghwan <odh1995@gmail.com>
2017-06-12 21:16:53 +0900

    telegram bot 추가 및 작동

--- AdsCatcher/blog/views.py
+++ AdsCatcher/blog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Blog
 from allauth.socialaccount.models import SocialAccount, SocialToken
+#for telegram bot sending message
+import telegram 
+
+bot = telegram.Bot(token='384831612:AAF_QK-qKzIdYbgEJ6ksjjckTxU_GabhlIo')
+chat_id = bot.get_updates()[-1].message.chat.id
 # Create your views here.
 def login_index(request):
     blog_list = Blog.objects.all().order_by("-created_at")
@@ -13,6 +18,7 @@ def login_index(request):
         if blog.created_at == compared_created_time:
             if blog.feed_title != compared_title:
                 blog.state = "modified"
+                bot.sendMessage(chat_id=chat_id, text='광고를 잡은거 같아요!')
                 Blog.objects.filter(feed_title = str(compared_title)).delete()
             else:
                 pass
